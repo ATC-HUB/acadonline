7ac83b62a6eee56abb45e491a1bc6095efec9eae
habeebllah <lasisihabeeb67@gmail.com>
2022-01-18 17:00:54 +0100

    updates

--- components/EightSection.vue
+++ components/EightSection.vue
@@ -1,77 +1,142 @@
 <template>
-    <div class="section section-padding-02 mt-n1 mb-5">
-            <div class="container">
+  <div class="section section-padding-02 mt-n1 mb-5">
+    <div class="container">
+      <!-- Section Title Start -->
+      <div class="section-title text-center">
+        <h2 class="main-title"><span> Student Testimonial</span></h2>
 
-                <!-- Section Title Start -->
-                <div class="section-title shape-03 text-center">
-                    <h2 class="main-title"> <span> Student Testimonial</span></h2>
+        <h5 class="sub-title" style="color: black">
+          Feedback from our Students
+        </h5>
+      </div>
+      <!-- Section Title End -->
 
-                    <h5 class="sub-title" style="color: black;">Feedback from our Students</h5>
-                </div>
-                <!-- Section Title End -->
-
-                <!-- Testimonial Wrapper End -->
-                <div class="testimonial-wrapper testimonial-active">
-                    <div class="swiper">
-              <div class="swiper-wrapper">
-                <div v-for="i in 6" :key="i" class="swiper-slide" :class="`slide--${i}`">
-                  <div class="slider-content">Slide {{ i }}</div>
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="slider-content">
+              <div class="row">
+                <div class="content-container">
+                  <h6>
+                    You are very close to become the person of your dream by
+                    taking any of our online/offline tech track courses Read
+                    More
+                  </h6>
+                  <span>Dada Adepoju (UX/UI)</span>
                 </div>
               </div>
-              <!-- If pagination is needed -->
-              <div class="swiper-pagination"></div>
-
-              <!-- If navigation buttons are needed -->
-              <div class="swiper-button-prev"></div>
-              <div class="swiper-button-next"></div>
-            </div>                        
-                </div>
-                <!-- Testimonial Wrapper End -->
-
             </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="slider-content">
+              <div class="row">
+                <div class="content-container">
+                  <h6>
+                    You are very close to become the person of your dream by
+                    taking any of our online/offline tech track courses Read
+                    More
+                  </h6>
+                  <span>Dada Adepoju (UX/UI)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="slider-content">
+              <div class="row">
+                <div class="content-container">
+                  <h6>
+                    You are very close to become the person of your dream by
+                    taking any of our online/offline tech track courses Read
+                    More
+                  </h6>
+                  <span>Dada Adepoju (UX/UI)</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <!-- If pagination is needed -->
+        <div class="swiper-pagination"></div>
+
+        <!-- If navigation buttons are needed -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { Swiper, Navigation, Pagination, Autoplay } from 'swiper'
-import 'swiper/swiper-bundle.min.css'
-export default{
-    name: "EightSection",
-    mounted() {
+import { Swiper, Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/swiper-bundle.min.css";
+export default {
+  name: "EightSection",
+  mounted() {
     // configure Swiper to use modules. The modules were tested with SwiperJS v6.8.4 with NuxtJS v2.15.7
     // previously it was before export default. Moved here for performance issues. Move back in case of problems.
     // add or remove unused modules
-    Swiper.use([Navigation, Pagination, Autoplay])
-    
+    Swiper.use([Navigation, Pagination, Autoplay]);
+
     // init Swiper:
     /* eslint-disable no-unused-vars */
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper(".swiper", {
       // Optional parameters
       // @see https://swiperjs.com/swiper-api#parameters
-      direction: 'horizontal',
+      direction: "horizontal",
       loop: true,
       // remove unused modules if needed
       modules: [Navigation, Pagination, Autoplay],
       // Pagination if needed
       pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
       },
       // Autoplay if needed
       autoplay: {
-        delay: 3000
+        delay: 3000,
       },
       // Navigation arrows if needed
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      }
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
       // Configure other options. Not tested
-    })}
-}
+    });
+  },
+};
 </script>
 
 <style scoped>
+.swiper {
+  height: 500px;
+}
+.swiper-slide {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
 
+.content-container {
+  border: 1px solid #ded9d9;
+  border-radius: 35px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.3);
+  padding: 20px;
+  height: fix-content;
+}
+
+.content-container h6 {
+  padding-top: 40px;
+  padding-bottom: 30px;
+  color: #150c21b2;
+  line-height: 1.7;
+  font-size: 16px;
+  width: 100%;
+}
+.content-container span {
+  border-top: 1px solid #000000;
+  font-weight: 500;
+  margin-bottom: 50px;
+}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/eslint-module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
