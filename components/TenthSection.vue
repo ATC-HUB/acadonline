f9f76691249a9e48322ee6d69a954cf6003f49b0
Habeebllah <lasisihabeeb67@gmail.com>
2022-02-01 14:16:01 +0100

    update

--- components/TenthSection.vue
+++ components/TenthSection.vue
@@ -38,6 +38,10 @@
 
           <div class="swiper-pagination"></div>
         </div>
+
+        <div class="row b-btn">
+          <button class="btn">Read More</button>
+      </div>
       </div>
     </div>
   </div>
@@ -123,6 +127,26 @@ export default {
   z-index: 1;
   padding-left: 240px;
   padding-right: 240px;
+}
+
+.btn{
+    color: #fff;
+    border: 2px solid #fff;
+    border-radius: 10px;
+    padding: 20px 60px;
+    margin-top: 50px;
+    background: #150C21E5;
+    width: 20%;
+}
+
+.b-btn{
+    justify-content: center;
+}
+
+.btn:hover{
+    color: #150C21E5;
+    background: #fff;
+    border: 2px solid #150C21E5;
 }
 
 </style>
