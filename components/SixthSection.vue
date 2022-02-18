68ae92dff100064cb633119ee59b5a88bd0c5f21
Habeebllah <lasisihabeeb67@gmail.com>
2022-02-18 14:57:55 +0100

    sixthsection responsiveness fixed

--- components/SixthSection.vue
+++ components/SixthSection.vue
@@ -4,16 +4,17 @@
       <div class="row youtube-video-section">
         <img src="assets/images/mob-bg.png" class="mb-img">
         <img src="assets/images/backg.png" class="b-img" />
-        <div class="col-sm-5">
+        <div class="col-sm-5 design_school_desk">
           <h4>Design School Presentation</h4>
           <p>
             You are very close to become the person of your dream by taking any
             of our online/ offline tech track courses <br />Read More
           </p>
         </div>
-        <div class="col-sm-7">
+
+        <div class="col-sm-7 slider_desk">
           <h5>MOMENTS FROM THE INSTITUTE</h5>
-          <h3>Hands on Project Defence <br />in the course of studying</h3>
+          <h3>Hands on Project Defence <br class="breaks"/>in the course of studying</h3>
           <div class="swiper-c mt-5">
             <div class="swiper">
               <div class="swiper-wrapper">
@@ -59,6 +60,64 @@
               <div class="swiper-button-next"></div>
             </div>
           </div>
+        </div>
+        <div class="col-sm-7 slider_mob">
+          <h5 class="mob_text2">MOMENTS FROM THE INSTITUTE</h5>
+          <h3 class="desk_text">Hands on Project Defence <br />in the course of studying</h3>
+          <h4 class="mob_text">Hands on Project Defence in the course of studying</h4>
+
+          <div class="swiper-c mt-5">
+            <div class="swiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <iframe
+                      width="100%"
+                      height="500px"
+                      src="https://www.youtube.com/embed/rjrPx_2GzyU"
+                      title="YouTube video player"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen
+                    ></iframe>
+                </div>
+                <div class="swiper-slide">
+                    <iframe
+                      width="100%"
+                      height="500px"
+                      src="https://www.youtube.com/embed/GUe9yKnSsYE"
+                      title="YouTube video player"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen
+                    ></iframe>
+                </div>
+                <div class="swiper-slide"> 
+                    <iframe
+                      width="100%"
+                      height="500px"
+                      src="https://www.youtube.com/embed/RUt-ySO5du0"
+                      title="YouTube video player"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen
+                    ></iframe>
+                </div>
+              </div>
+              <!-- If pagination is needed -->
+              <div class="swiper-pagination"></div>
+
+              <!-- If navigation buttons are needed -->
+              <div class="swiper-button-prev"></div>
+              <div class="swiper-button-next"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5 design_school_mob">
+          <h4><span class="small_box"></span> Design School Presentation</h4>
+          <p>
+            You are very close to become the person of your dream by taking any
+            of our online/ offline tech track courses <br />Read More
+          </p>
         </div>
       </div>
     </div>
@@ -115,9 +174,13 @@ export default {
   height: 810px;
 }
 
+.small_box{
+  display: none;
+}
 
 .youtube-video-section {
   height: 850px;
+  position: relative;
 }
 
 .swiper-button-next, .swiper-button-prev {
@@ -164,10 +227,9 @@ export default {
   font-weight: 500;
 }
 
-.mb-img{
+.mb-img, .design_school_mob, .slider_mob, .mob_text{
   display: none;
 }
-
 .swiper {
   height: 100%;
   overflow: hidden;
@@ -192,7 +254,66 @@ export default {
     z-index: -1;
     position: absolute;
     padding: 0;
-    height: 980px;
+    height: 1080px;
   }
+  .design_school_mob, .slider_mob, .mob_text{
+    display: block;
+  }
+  
+  .section-padding-02{
+    margin-bottom: 170px;
+  }
+  .small_box{
+    display: block;
+    width: 15px;
+    height: 15px;
+    background-color: #1207ab;
+    align-self: center;
+    margin-right: 15px;
+
+  }
+  .design_school_desk, .slider_desk, .desk_text{
+    display: none;
+  }
+
+  .design_school_mob h4{
+    display: flex;
+    font-size: 18px;
+  }
+
+
+.col-sm-7 h5 {
+  margin-top: 140px;
+  
+}
+
+  .mob_text{
+    font-size: 30px;
+    color: #1207ab;
+    font-weight: 500;
+    margin-top: 40px
+  }
+
+  .design_school_mob h4{
+    margin-top: 40px;
+  }
+  .swiper-c {
+  width: 100%;
+  height: 290px;
+}
+
+.swiper-slide iframe{
+  height: inherit
+}
+
+.breaks{
+  display: none;
+}
+
+.col-sm-5 p{
+  padding: 0px;
+  margin-top: 40px;
+}
+
 }
 </style>
